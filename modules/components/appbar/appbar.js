d60d654a0338632999f2daf7680397287f8c7949
monodnb <monodnb@gmail.com>
2015-06-29 03:29:17 +0300

    started with appbar

--- modules/components/appbar/appbar.js
+++ modules/components/appbar/appbar.js
@@ -27,32 +27,96 @@
         self.init($element);
     }
 
-    // Update Attributes
-    AppBar.prototype.updateAttrs = function($object, opt) {
-        $.each(opt, function(key, value) {
-            if (!(key.indexOf("gd-") > -1)) {
-                $object.attr(key, value);
+    // Get Nav links
+    AppBar.prototype.getNav = function($element, $gdelem) {
+        // console.log($element.parent())
+        if ($gdelem.length > 0) {
+            var $parent = $element.parent();
+            $element = $element.detach().clone();
+            for (var a = 0; a < $gdelem.length; a++) {
+                //$element.appendTo($parent);
+                // var attributes = $element[a].attributes,
+                //     i = attributes.length;
+                // while (i--) {
+                //     $element[a].removeAttributeNode(attributes[i]);
+                // }
+                //self.getAttrs($element, $gdelem.eq(a))
             }
-        });
+        }
+        //console.log($element.closest('nav').find('> ul > li').not('.app-logo'));
     }
 
-    //Init
-    AppBar.prototype.init = function($element) {
-        //initial setup
-        //clear the element
-        //$element.empty();
-        //get the template and store it in a jquery object
-        // $.get("./modules/components/appbar/appbar.template.html", function(data) {
-        //     $element.html(data);
-        // 
-        //     var $appBar = $element.find("#appbar"),
-        //         $appTitle = $element.find(".app-title"),
-        //         $appNav = $element.find("nav"),
-        //         navLinks = '<li class="tab"><a></a></li>'
-        // 
-        //     self.updateAttrs($appBar, self.options);
-        // });
-        console.log(self.options);
+    // Get html
+    AppBar.prototype.getHtml = function($element, $gdelem) {
+            if ($gdelem.length > 0) {
+                $element.html($gdelem.html());
+            }
+        }
+        // Get attributes
+    AppBar.prototype.getAttrs = function($element, $gdelem) {
+        if ($gdelem.length > 0) {
+            var attributes = $gdelem[0].attributes,
+                aLength = attributes.length;
+            for (var a = 0; a < aLength; a++) {
+                var key = attributes[a].name.toLowerCase(),
+                    value = attributes[a].value;
+                $element.attr(key, value);
+            }
+        }
+    }
+
+    // Construct
+    AppBar.prototype.construct = function($element, $gdelem) {
+        var $elAppBar = $element.find('#appbar'),
+            $elAppTitle = $element.find('h1.app-title'),
+            $gdAppTitle = $gdelem.find('app-title'),
+            $elAppViewTitle = $element.find('h2.app-view-title'),
+            $gdAppViewTitle = $gdelem.find('app-view-title'),
+            $elAppNav = $element.find('nav'),
+            $gdAppNav = $gdelem.find('app-nav'),
+            $elNavLink = $element.find('nav > ul > li').not('.app-logo'),
+            $gdNavLink = $gdelem.find('app-link');
+
+        // app bar
+        self.getAttrs($elAppBar, $gdelem);
+        // app title
+        self.getHtml($elAppTitle, $gdAppTitle);
+        self.getAttrs($elAppTitle, $gdAppTitle);
+        // app view title
+        self.getHtml($elAppViewTitle, $gdAppViewTitle);
+        self.getAttrs($elAppViewTitle, $gdAppViewTitle);
+        // app nav
+        self.getAttrs($elAppNav, $gdAppNav);
+        self.getNav($elNavLink, $gdNavLink);
+    }
+
+    // Template
+    AppBar.prototype.template = function($element, $gdelem) {
+        $element.html(
+            '<div id="appbar">' +
+            '<h1 class="app-title"></h1>' +
+            '<div id="appbar-nav">' +
+            '<nav>' +
+            '<h2>Main Navigation</h2>' +
+            '<ul class="tabs-list">' +
+            '<li class="tab app-logo"><a href="/"></a></li>' +
+            '<span flex></span>' +
+            '<li class="tab"><a href=""></a></li>' +
+            '</ul>' +
+            '</nav>' +
+            '</div>' +
+            '<div id="appbar-extension">' +
+            '<h2 class="app-view-title"></h2>' +
+            '</div>' +
+            '</div>'
+        )
+        self.construct($element, $gdelem);
+    }
+
+    // Init
+    AppBar.prototype.init = function($element, $gdelem) {
+        var $gdelem = $element.clone();
+        self.template($element, $gdelem);
     };
 
     //create the AppBar instance
