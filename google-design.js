2dd7152a9e455bccef895f4527521832d1544547
monodnb <monodnb@gmail.com>
2015-06-29 15:10:47 +0300

    restructuring in modules, more work on gd-appbar. Need to expand the template

--- google-design.js
+++ google-design.js
@@ -1,9 +1,9 @@
-(function(init) {
+(function (init) {
     'use strict';
     // The global jQuery object is passed as a parameter
     init(window.jQuery, window, document);
 
-}(function($, window, document) {
+}(function ($, window, document) {
     'use strict';
 
     //plugin name
@@ -28,32 +28,43 @@
     }
 
     // Get Nav links
-    AppBar.prototype.getNav = function($element, $gdelem) {
+    AppBar.prototype.getNav = function ($element, $gdelem) {
         // console.log($element.parent())
         if ($gdelem.length > 0) {
             var $parent = $element.parent();
-            $element = $element.detach().clone();
+            $element.detach();
             for (var a = 0; a < $gdelem.length; a++) {
-                //$element.appendTo($parent);
-                // var attributes = $element[a].attributes,
-                //     i = attributes.length;
-                // while (i--) {
-                //     $element[a].removeAttributeNode(attributes[i]);
-                // }
-                //self.getAttrs($element, $gdelem.eq(a))
+                var attributes = $element[0].attributes,
+                    i = attributes.length;
+                while (i--) {
+                    $element[0].removeAttributeNode(attributes[i]);
+                }
+                self.getAttrs($element, $gdelem.eq(a))
+                var className = $element[0].className;
+                if (className !== ''){
+                    $element[0].className = ('tab ' + className);
+                }else {
+                    $element[0].className = 'tab';
+                }
+                var href = $element.attr('href'),
+                    $anchor = $element.find('>a');
+                $element.removeAttr('href');
+                $anchor.attr('href', href);
+                self.getHtml($anchor, $gdelem.eq(a));
+                $element.clone().appendTo($parent);
             }
         }
         //console.log($element.closest('nav').find('> ul > li').not('.app-logo'));
     }
 
     // Get html
-    AppBar.prototype.getHtml = function($element, $gdelem) {
+    AppBar.prototype.getHtml = function ($element, $gdelem) {
             if ($gdelem.length > 0) {
                 $element.html($gdelem.html());
             }
         }
         // Get attributes
-    AppBar.prototype.getAttrs = function($element, $gdelem) {
+    AppBar.prototype.getAttrs = function ($element, $gdelem) {
         if ($gdelem.length > 0) {
             var attributes = $gdelem[0].attributes,
                 aLength = attributes.length;
@@ -66,7 +77,7 @@
     }
 
     // Construct
-    AppBar.prototype.construct = function($element, $gdelem) {
+    AppBar.prototype.construct = function ($element, $gdelem) {
         var $elAppBar = $element.find('#appbar'),
             $elAppTitle = $element.find('h1.app-title'),
             $gdAppTitle = $gdelem.find('app-title'),
@@ -91,37 +102,38 @@
     }
 
     // Template
-    AppBar.prototype.template = function($element, $gdelem) {
+    AppBar.prototype.template = function ($element, $gdelem) {
         $element.html(
             '<div id="appbar">' +
-            '<h1 class="app-title"></h1>' +
-            '<div id="appbar-nav">' +
-            '<nav>' +
-            '<h2>Main Navigation</h2>' +
-            '<ul class="tabs-list">' +
-            '<li class="tab app-logo"><a href="/"></a></li>' +
-            '<span flex></span>' +
-            '<li class="tab"><a href=""></a></li>' +
-            '</ul>' +
-            '</nav>' +
-            '</div>' +
-            '<div id="appbar-extension">' +
-            '<h2 class="app-view-title"></h2>' +
-            '</div>' +
+                '<h1 class="app-title off-screen"></h1>' +
+                '<div id="appbar-nav">' +
+                    '<nav>' +
+                        '<h2 class="off-screen">Main Navigation</h2>' +
+                        '<ul class="tabs-list">' +
+                            '<li class="tab app-logo"><a href="/"></a></li>' +
+                            '<span flex></span>' +
+                            '<li class="tab"><a href=""></a></li>' +
+                        '</ul>' +
+                    '</nav>' +
+                    '<div class="app-menu"></div>' +
+                '</div>' +
+                '<div id="appbar-extension">' +
+                    '<h2 class="app-view-title"></h2>' +
+                '</div>' +
             '</div>'
         )
         self.construct($element, $gdelem);
     }
 
     // Init
-    AppBar.prototype.init = function($element, $gdelem) {
+    AppBar.prototype.init = function ($element, $gdelem) {
         var $gdelem = $element.clone();
         self.template($element, $gdelem);
     };
 
     //create the AppBar instance
     var gdAppbarProto = Object.create(HTMLElement.prototype);
-    gdAppbarProto.attachedCallback = function() {
+    gdAppbarProto.attachedCallback = function () {
         var $element = $(this),
             options = {},
             attributes = this.attributes,
--- gulp/styles.js
+++ gulp/styles.js
@@ -12,15 +12,15 @@ module.exports = function(options) {
             style: 'expanded'
         };
 
-        var variables = gulp.src([options.app + 'modules/variables/**/*.scss'], {read: false});
-        var core = gulp.src([options.app + 'modules/core/**/*.scss'], {read: false});
-        var layout = gulp.src([options.app + 'modules/layout/**/*.scss'], {read: false});
-        var animations = gulp.src([options.app + 'modules/animations/**/*.scss'], {read: false});
+        var variables = gulp.src([options.app + 'modules/core/styles/variables/**/*.scss'], {read: false});
+        var base = gulp.src([options.app + 'modules/core/styles/base/**/*.scss'], {read: false});
+        var layout = gulp.src([options.app + 'modules/core/styles/layout/**/*.scss'], {read: false});
+        var animations = gulp.src([options.app + 'modules/core/styles/animations/**/*.scss'], {read: false});
         var components = gulp.src([options.app + 'modules/components/**/*.scss'], {read: false});
 
         var injectOptions = {
             transform: function(filePath) {
-                filePath = filePath.replace(options.app + '/styles/', '');
+                //filePath = filePath.replace(options.app + '/styles/', '');
                 return '@import \'' + filePath + '\';';
             },
             starttag: '// injector',
@@ -32,7 +32,8 @@ module.exports = function(options) {
         return gulp.src([
                 options.app + 'modules/google-design.scss'
             ])
-            .pipe($.inject(series(variables, core, layout, animations, components), injectOptions))
+            .pipe($.inject(series(variables, base, layout, animations, components), injectOptions))
+            //.pipe(gulp.dest(options.app + 'modules/'))
             .pipe($.sass(sassOptions)).on('error', options.errorHandler('Sass'))
             .pipe($.autoprefixer({
                 browsers: ['last 2 versions'],
